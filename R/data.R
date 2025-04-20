7d18af873e99efe3c4abdd205eed5a52d4b6870d
jess <jessachandler@gmail.com>
2025-04-20 07:37:43 -0700

    adds data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Olympic National Park Trails
+#'
+#' A dataset containing geographic feature lines representing trails
+#' in Olympic National Park
+#' @export
+#' @format An object of class \code{SimpleFeaturesDataFrame}
+#' @source \url{URL to your source data, if applicable}
+"onp_trails"
+
+#' Olympic National Forest Trails
+#'
+#' A dataset containing geographic feature lines representing trails
+#' in Olympic National Forest
+#' @export
+#' @format An object of class \code{SimpleFeaturesDataFrame}
+#' @source \url{URL to your source data, if applicable}
+"onf_trails"
