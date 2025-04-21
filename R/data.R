f9cedf15277aeb234b818559e47452069145dbba
jess <jessachandler@gmail.com>
2025-04-21 16:25:24 -0700

    fix data export

--- R/data.R
+++ R/data.R
@@ -2,7 +2,6 @@
 #'
 #' A dataset containing geographic feature lines representing trails
 #' in Olympic National Park
-#' @export
 #' @format An object of class \code{SimpleFeaturesDataFrame}
 #' @source \url{URL to your source data, if applicable}
 "onp_trails"
@@ -11,7 +10,6 @@
 #'
 #' A dataset containing geographic feature lines representing trails
 #' in Olympic National Forest
-#' @export
 #' @format An object of class \code{SimpleFeaturesDataFrame}
 #' @source \url{URL to your source data, if applicable}
 "onf_trails"
