6232cfebe5514df63d12c6d1294473674e8dc42b
jess <jessachandler@gmail.com>
2025-05-08 17:04:57 -0700

    adds function get_trails

---
+++ R/get_trails.R
@@ -0,0 +1,34 @@
+
+#' Get Trails for a Track
+#'
+#' Pulls trails from Open Street Map for
+#' the bounding box of the passed tracks.
+#'
+#' @details
+#' subjet to all memory limits of Overpass
+#' queries through osmdata
+#'
+#' @importFrom osmdata opq add_osm_feature osmdata_sf
+#'
+#' @param track_sf
+#'
+#' @return
+#' @export
+#'
+#' @examples \dontrun{
+#' this_track <- read_geo(trailcover_example("Lake_Angeles.gpx"))
+#' this_trails <- get_trails(this_track)
+#' }
+get_trails <- function(track_sf){
+  this_bb <-  get_bbox(track_sf)
+
+  this_go <- this_bb %>%
+    osmdata::opq() %>%
+    osmdata::add_osm_feature(key = "highway") %>%
+    osmdata::osmdata_sf()
+
+  this_trails <- this_go$osm_lines[grepl("Trail",
+                                         this_go$osm_lines$name),]
+  this_trails <- this_trails[c('name','geometry')]
+  return(this_trails)
+}
